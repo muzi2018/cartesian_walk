89489f92346a59c724224b6108ce3b7669bb9da4
Weijie Wang <duangsir518@163.com>
2024-05-08 14:46:51 +0200

    removing tag
    keep running

--- src/walking_node.cpp
+++ src/walking_node.cpp
@@ -35,22 +35,19 @@ bool start_walking(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res
 
 void tagDetectionsCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
 {
+    std::cout << "msg->detections.size() = " << msg->detections.size() << std::endl;\
 
-    
-
-    for (const auto& detection : msg->detections) {
-        // Check if the tag is detected
-        if (detection.id.size() > 0) {
-            tagDetected = true;
-            break; // Exit the loop once a tag is found
-        }
+    if (msg->detections.size() > 0) {
+        tagDetected = true;
+    }else{
+        tagDetected = false;
     }
 
     if (tagDetected) {
         ROS_INFO("A tag has been detected.");
         // Perform actions based on the detection
     } else {
-        ROS_INFO("No tags detected.");
+        ROS_INFO("No tags detected. tagDetected = %d", tagDetected);
     }
 
 }
@@ -72,22 +69,8 @@ int main(int argc, char **argv)
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
 
-
-    // Get node parameters
-    // std::string URDF_PATH, SRDF_PATH;
-    // nodeHandle.getParam("/urdf_path", URDF_PATH);
-    // nodeHandle.getParam("/srdf_path", SRDF_PATH);
     auto cfg = XBot::ConfigOptionsFromParamServer();
-    // an option structure which is needed to make a model
-    // XBot::ConfigOptions xbot_cfg;
-    // // set the urdf and srdf path..
-    // xbot_cfg.set_urdf_path(URDF_PATH);
-    // xbot_cfg.set_srdf_path(SRDF_PATH);
-    // // the following call is needed to generate some default joint IDs
-    // xbot_cfg.generate_jidmap();
-    // // some additional parameters..
-    // xbot_cfg.set_parameter("is_model_floating_base", true);
-    // xbot_cfg.set_parameter<std::string>("model_type", "RBDL");
+
 
     // and we can make the model class
     auto model = XBot::ModelInterface::getModel(cfg);
@@ -95,7 +78,6 @@ int main(int argc, char **argv)
     // initialize to a homing configuration
     Eigen::VectorXd qhome;
     model->getRobotState("home", qhome);
-    // qhome.setZero(); 
     model->setJointPosition(qhome);
     model->update();
     XBot::Cartesian::Utils::RobotStatePublisher rspub (model);
@@ -132,129 +114,24 @@ int main(int argc, char **argv)
     Eigen::VectorXd q, qdot, qddot;
     auto car_task = solver->getTask("base_link");
     auto car_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(car_task);    
-    // auto right_arm_task = solver->getTask("arm2_8");
-    // auto rarm_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(right_arm_task);
 
 
-
-    // /**leg task*/
-    // auto leg1_task = solver->getTask("wheel_1");
-    // auto leg1_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(leg1_task);
-
-    // /**leg task*/
-    // auto leg2_task = solver->getTask("wheel_2");
-    // auto leg2_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(leg2_task);
-
-    // /**leg task*/
-    // auto leg3_task = solver->getTask("wheel_3");
-    // auto leg3_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(leg3_task);
-
-    // /**leg task*/
-    // auto leg4_task = solver->getTask("wheel_4");
-    // auto leg4_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(leg4_task);
-
-    // /**com task*/
-    // auto com_task = solver->getTask("com");
-    // auto com_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(com_task);
-
-    
-    // // // get pose reference from task
-    // // // ...
-    Eigen::Affine3d RightArm_T_ref;
-    Eigen::Affine3d Leg1_T_ref;
-    Eigen::Affine3d Leg2_T_ref;
-    Eigen::Affine3d Leg3_T_ref;
-    Eigen::Affine3d Leg4_T_ref;
-    Eigen::Affine3d Com_T_ref;
-    
-    
-    double x, z;
-    int leg_state = 1,num_leg = 2, segment = 0;
-    double phase_time = 3, target_time = num_leg*phase_time;
-
-    // the foot position in world coordinate
-    Eigen::Vector3d leg1_pos;
-    const std::string leg1_frame = "wheel_1";
-    model->getPointPosition(leg1_frame, Eigen::Vector3d::Zero(),leg1_pos); 
-
-    Eigen::Vector3d leg2_pos;
-    const std::string leg2_frame = "wheel_2";
-    model->getPointPosition(leg2_frame, Eigen::Vector3d::Zero(),leg2_pos); 
-
-    Eigen::Vector3d leg3_pos;
-    const std::string leg3_frame = "wheel_3";
-    model->getPointPosition(leg3_frame, Eigen::Vector3d::Zero(),leg3_pos); 
-
-    Eigen::Vector3d leg4_pos;
-    const std::string leg4_frame = "wheel_4";
-    model->getPointPosition(leg4_frame, Eigen::Vector3d::Zero(),leg4_pos); 
-
-    Eigen::Vector3d leg_mid;
-    leg_mid = leg1_pos + leg2_pos + leg3_pos + leg4_pos;
-
-    Eigen::Vector3d com_pos;
-    model->getCOM(com_pos);
-
-    ROS_INFO_STREAM("wheel_1");
-    ROS_INFO_STREAM(leg1_pos);
-    ROS_INFO_STREAM("wheel_2");
-    ROS_INFO_STREAM(leg2_pos);
-    ROS_INFO_STREAM("wheel_3");
-    ROS_INFO_STREAM(leg3_pos);
-    ROS_INFO_STREAM("wheel_4");
-    ROS_INFO_STREAM(leg4_pos);
-    ROS_INFO_STREAM("com_pos");
-    ROS_INFO_STREAM(com_pos);
-
-
-    // Trajectory::WayPointVector wp;
-    // Eigen::Affine3d w_T_f1 ;
-    // w_T_f1.setIdentity();
-
-    // w_T_f1.pretranslate(Eigen::Vector3d(0.2,0,0));
-    // wp.emplace_back(w_T_f1,10);
-    // leg1_cartesian->setWayPoints(wp);
-    
-    /** task*/
-    int current_state1 = 0;
-    int current_state2 = 0;
-    int current_state3 = 0;
-    int current_state4 = 0;
-
-
-    bool state1_support = false;
-    bool state2_support = false;
-    bool state3_support = false;
-    bool state4_support = false;
-
-    int i=1; // mpc step index 
-    double long_x = 0.1; // step long distance
-    double long_y = 0;
-    double leg_height = 0.1; // step height
-    double seg_num = 1000; // mpc segment number
-    double seg_time = phase_time / seg_num; // mpc segment duration
-    double seg_dis = long_x / seg_num; // each mpc step long
-
-    double com_shift_x, com_shift_y;
     double det_shift_x = 0.5, det_shift_y = 0.5;
 
     ros::Rate r(100);
     ros::ServiceServer service = nodeHandle.advertiseService("start_walking", start_walking);
 
-    // D435_head_camera_color_optical_frame
-    // tag_0
+    // // D435_head_camera_color_optical_frame
+    // // tag_0
+    // ros::Publisher pub_x_d = nodeHandle.advertise<std_msgs::Float64>("x_desire", 1000);
+    // std_msgs::Float64 msg_xd;
+    // ros::Publisher pub_x_c = nodeHandle.advertise<std_msgs::Float64>("x_actual", 1000);
+    // std_msgs::Float64 msg_xc;
 
-    ros::Publisher pub_x_d = nodeHandle.advertise<std_msgs::Float64>("x_desire", 1000);
-    std_msgs::Float64 msg_xd;
-    ros::Publisher pub_x_c = nodeHandle.advertise<std_msgs::Float64>("x_actual", 1000);
-    std_msgs::Float64 msg_xc;
-
-    ros::Publisher pub_y_d = nodeHandle.advertise<std_msgs::Float64>("y_desire", 1000);
-    std_msgs::Float64 msg_yd;
-    ros::Publisher pub_y_c = nodeHandle.advertise<std_msgs::Float64>("y_actual", 1000);
-    std_msgs::Float64 msg_yc;
-
-    Eigen::Vector3d temp ;
+    // ros::Publisher pub_y_d = nodeHandle.advertise<std_msgs::Float64>("y_desire", 1000);
+    // std_msgs::Float64 msg_yd;
+    // ros::Publisher pub_y_c = nodeHandle.advertise<std_msgs::Float64>("y_actual", 1000);
+    // std_msgs::Float64 msg_yc;
     
 
     geometry_msgs::TransformStamped tag_base_T; 
@@ -269,94 +146,60 @@ int main(int argc, char **argv)
         }
 
 
-        // Define the parent and child frame names
+        /**
+         * Detection
+        */
         std::string parent_frame = "base_link";
         std::string child_frame = "tag_0";
-        // Query the transformation
-        try {
+
+        if (tagDetected)
+        {
+            std::cout << "tagDetected = " << tagDetected << std::endl;
             tag_base_T = tfBuffer.lookupTransform(parent_frame, child_frame, ros::Time(0));
-            
-            // ROS_INFO("Transformation from %s to %s: ", parent_frame.c_str(), child_frame.c_str());
-            // ROS_INFO("Translation: x=%f, y=%f, z=%f", tag_base_T.transform.translation.x, tag_base_T.transform.translation.y, tag_base_T.transform.translation.z);
-            
-            // ROS_INFO("Rotation: w=%f, x=%f, y=%f, z=%f", tag_base_T.transform.rotation.w, tag_base_T.transform.rotation.x, tag_base_T.transform.rotation.y, tag_base_T.transform.rotation.z);
+            /**
+             * Error Calculate
+            */
+            double x_e = tag_base_T.transform.translation.x * tag_base_T.transform.translation.x;
+            double y_e = tag_base_T.transform.translation.y * tag_base_T.transform.translation.y;
+            double z_e = tag_base_T.transform.translation.z * tag_base_T.transform.translation.z;
+            double e = sqrt(x_e + y_e);
 
-            
-            
-        } catch (tf2::LookupException &ex) {
-            ROS_ERROR("TF Exception: %s", ex.what());
-            // ROS_INFO("detection: %d ", detection);
+            /**
+             * Velocity Controller
+            */
+            Eigen::Vector6d E;
+            double K = 0.6;
+            E[0] = K * tag_base_T.transform.translation.x;
+            E[1] = K * tag_base_T.transform.translation.y;
+            E[2] = 0;
+            E[3] = 0;
+            E[4] = 0;
+            E[5] = 0;
+            E = K * E * e;
+            Eigen::Vector6d E_Zero;
+            E_Zero.setZero();
+
+
+
+            if (e >= 1)
+            {
+                car_cartesian->setVelocityReference(E);
+            }else {
+                std::cout << "Reach goal" << std::endl;
+                car_cartesian->setVelocityReference(E_Zero);
+            }
         }
-
-
-        auto tag_base_p = tag_base_T.transform.translation;
-        tag_base_p.x = tag_base_p.x - det_shift_x ;
-        tag_base_p.y = tag_base_p.y - det_shift_y ;
-        
-        Eigen::Vector3d base_pos;
-        Eigen::Vector3d base_pos_d;
-        Eigen::Vector3d target_in_base;
-        base_pos_d << tag_base_T.transform.translation.x, tag_base_T.transform.translation.y,tag_base_T.transform.translation.z; 
-        const std::string base_frame = "base_link";
-        
-        model->getPointPosition(base_frame, Eigen::Vector3d::Zero(), base_pos); 
-        model->getPointPosition(base_frame, target_in_base, base_pos_d); 
-
-        msg_xd.data = base_pos_d[0];
-        pub_x_d.publish(msg_xd);
-        msg_xc.data = base_pos[0];
-        pub_x_c.publish(msg_xc);
-        
-        msg_yd.data = base_pos_d[1];
-        pub_y_d.publish(msg_yd);
-        msg_yc.data = base_pos[1];
-        pub_y_c.publish(msg_yc);
-
-
-
-
-
-
-        // ROS_INFO("Tracking Error: %d", msg_x);
-        double x_e = tag_base_T.transform.translation.x * tag_base_T.transform.translation.x;
-        double y_e = tag_base_T.transform.translation.y * tag_base_T.transform.translation.y;
-        double z_e = tag_base_T.transform.translation.z * tag_base_T.transform.translation.z;
-
-        // ROS_INFO("Translation: x=%f, y=%f, z=%f", tag_base_T.transform.translation.x, tag_base_T.transform.translation.y, tag_base_T.transform.translation.z);
-        // ROS_INFO("Rotation: w=%f, x=%f, y=%f, z=%f", tag_base_T.transform.rotation.w, tag_base_T.transform.rotation.x, tag_base_T.transform.rotation.y, tag_base_T.transform.rotation.z);
-
-        tf::Quaternion q_R(tag_base_T.transform.rotation.w, tag_base_T.transform.rotation.x, tag_base_T.transform.rotation.y, tag_base_T.transform.rotation.z);
-
-
-
-        double e = sqrt(x_e + y_e);
-        Eigen::Vector6d E;
-        double K = 0.6;
-        E[0] = K * tag_base_T.transform.translation.x;
-        E[1] = K * tag_base_T.transform.translation.y;
-        E[2] = 0;
-
-        E[3] = 0;
-        E[4] = 0;
-        E[5] = 0;
-        E = K * E * e;
-
-        Eigen::Vector6d E_Zero;
-        E_Zero.setZero();
-
-        if (tagDetected && e >= 1){
-            car_cartesian->setVelocityReference(E);
-        } else{
+        if (!tagDetected){
+            std::cout << "tagDetected = " << tagDetected << std::endl;
+            Eigen::Vector6d E_Zero;
+            E_Zero.setZero();
             car_cartesian->setVelocityReference(E_Zero);
-        }
-        
-        // else 
-        // {
-        //     car_cartesian->setVelocityReference(E_Zero);
-        // }
-        
-        
-            // std::cout << "Motion started!" << std::endl;
+        } 
+
+        /**
+         * Move Robot
+        */
+
         solver->update(time, dt);
         model->getJointPosition(q);
         model->getJointVelocity(qdot);
@@ -370,32 +213,15 @@ int main(int argc, char **argv)
         robot->setPositionReference(q.tail(robot->getJointNum()));
         robot->setVelocityReference(qdot.tail(robot->getJointNum()));
         robot->move();
-        
+
         time += dt;
-
-
         rspub.publishTransforms(ros::Time::now(), "");
+        ros::spinOnce();
         r.sleep();
+        
     }
 }
 
-
-
-    // // Print position
-    // std::stringstream ss;
-    // ss << "qhome: [";
-    // for (int i = 0; i < qhome.size(); ++i)
-    // {
-        
-    //     if (i < qhome.size() - 1){
-    //         ss << "q" << i  << ": ";
-    //         ss << qhome[i];
-    //         ss << " ";
-    //     }
-
-    // }
-    // ss << "]";
-    // ROS_INFO_STREAM(ss.str());
 
 
 
