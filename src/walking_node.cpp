8686b7e90b670dd3e93dd19a08e41676898e3166
Weijie Wang <duangsir518@163.com>
2024-06-04 18:26:16 +0200

    turn to second tag

--- src/walking_node.cpp
+++ src/walking_node.cpp
@@ -20,9 +20,19 @@
 #include <Eigen/Dense>
 #include <std_srvs/Empty.h>
 #include <xbot_msgs/JointCommand.h>
+
 using namespace XBot::Cartesian;
 bool start_searching_bool = false;
-bool tagDetected = false;
+bool tagDetected_1 = false;
+bool tagDetected_2 = false;
+bool search_tag_2 = false;
+bool reach_goal_1 = false;
+bool reach_goal_2 = false;
+
+bool search_flag_2 = false;
+int off_num_2 = 70 ;
+
+const double dt = 0.01;
 bool start_searching(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
 {
     start_searching_bool = !start_searching_bool;
@@ -31,50 +41,40 @@ bool start_searching(std_srvs::Empty::Request& req, std_srvs::Empty::Response& r
 
 void tagDetectionsCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
 {
-    // std::cout << "msg->detections.size() = " << msg->detections.size() << std::endl;\
-
     if (msg->detections.size() > 0) {
-        tagDetected = true;
+        tagDetected_1 = true;
     }else{
-        tagDetected = false;
+        tagDetected_1 = false;
     }
-
-    // if (tagDetected) {
-    //     ROS_INFO("A tag has been detected.");
-    // } else {
-    //     ROS_INFO("No tags detected. tagDetected = %d", tagDetected);
-    // }
-
 }
 
-void SearchingTag(ros::NodeHandle nodeHandle){
-        std::vector<std::string> joint_names{
-                                            "torso_yaw",
-                                            };
-        std::vector<float> joint_positions (1);
-        joint_positions[0] = 0.5;
-        xbot_msgs::JointCommand joint_command;
-        ros::Publisher jointCmdPublisher_ = nodeHandle.advertise<xbot_msgs::JointCommand>("/xbotcore/command", 10);
-        joint_command.header.stamp = ros::Time::now();
-        joint_command.name = joint_names; 
-        joint_command.position = joint_positions;
-        joint_command.ctrl_mode = std::vector<uint8_t>(joint_positions.size(), 1);
-        jointCmdPublisher_.publish(joint_command);
-}
 
-void TurnToTag(XBot::Cartesian::CartesianTask* car_cartesian){
+void TurnToTag(XBot::Cartesian::CartesianTask* car_cartesian, int direction){
         Eigen::Vector6d E;
-        double yaw_e = 3.14 * 1/5;
+        double yaw_e = direction * 3.14 * 1/5;
         E[0] = 0;
         E[1] = 0;
         E[2] = 0;
         E[3] = 0;
         E[4] = 0;
-        E[5] = 0.5 * yaw_e;
+        E[5] = 0.2 * yaw_e;
         car_cartesian->setVelocityReference(E); 
         std::cout << "yaw_e = " << yaw_e << std::endl;
 }
-
+int turn_num = 60;
+void TurnAround(XBot::Cartesian::CartesianTask* car_cartesian, int & num){
+        Eigen::Vector6d E;
+        double yaw_e = -1 * 3.14 * 1/5;
+        E[0] = 0;
+        E[1] = 0;
+        E[2] = 0;
+        E[3] = 0;
+        E[4] = 0;
+        E[5] = 0.2 * yaw_e;
+        car_cartesian->setVelocityReference(E); 
+        num -- ;
+        std::cout << "TurnAround num = " << num << std::endl;
+}
 
 
 // Turn_yaw(E, int &tun_num, double K_yaw, XBot::Cartesian::CartesianTask* car_cartesian){
@@ -84,40 +84,26 @@ void TurnToTag(XBot::Cartesian::CartesianTask* car_cartesian){
 
 int main(int argc, char **argv)
 {
-    // std::cout << "robot->arm(1).getBaseLinkName() = " << robot->arm(1).getBaseLinkName() << std::endl;
-    std::cout << "wheel move move move = " << std::endl;
     const std::string robotName = "centauro";
     // Initialize ros node
     ros::init(argc, argv, robotName);
     ros::NodeHandle nodeHandle("");
-    // opendrawer
-    // opendoor
-    ros::ServiceClient client = nodeHandle.serviceClient<std_srvs::Empty>("opendoor");
+
     std_srvs::Empty srv;
-
-
-
     // Create a Buffer and a TransformListener
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
 
     auto cfg = XBot::ConfigOptionsFromParamServer();
-
-
     // and we can make the model class
     auto model = XBot::ModelInterface::getModel(cfg);
     auto robot = XBot::RobotInterface::getRobot(cfg);
     // initialize to a homing configuration
     Eigen::VectorXd qhome;
     model->getRobotState("home", qhome);
-    std::cout << "Homing configuration: " << std::endl << qhome.size() << std::endl;
-    std::cout << "qhome = " << qhome << std::endl;
-
     model->setJointPosition(qhome);
     model->update();
     XBot::Cartesian::Utils::RobotStatePublisher rspub (model);
-
-
     robot->setControlMode(
         {
             {"j_wheel_1", XBot::ControlMode::Velocity()},
@@ -127,15 +113,6 @@ int main(int argc, char **argv)
         }
     );
 
-    int homing_num = 100;
-    int searching_num = 0; bool search_flag = false, turn_num = 20;
-    bool home_flag = false;
-
-    // before constructing the problem description, let us build a
-    // context object which stores some information, such as
-    // the control period
-    const double dt = 0.01;
-    double time = 0, plan_time = 0;
     auto ctx = std::make_shared<XBot::Cartesian::Context>(
                 std::make_shared<XBot::Cartesian::Parameters>(dt),
                 model
@@ -144,7 +121,6 @@ int main(int argc, char **argv)
     // load the ik problem given a yaml file
     std::string problem_description_string;
     nodeHandle.getParam("problem_description", problem_description_string);
-
     auto ik_pb_yaml = YAML::Load(problem_description_string);
     XBot::Cartesian::ProblemDescription ik_pb(ik_pb_yaml, ctx);
 
@@ -152,48 +128,40 @@ int main(int argc, char **argv)
     auto solver = XBot::Cartesian::CartesianInterfaceImpl::MakeInstance("OpenSot",
                                                        ik_pb, ctx
                                                        );
-
-    
     ros::Subscriber sub = nodeHandle.subscribe("/tag_detections", 1000, tagDetectionsCallback);
-    
-
-
-    Eigen::VectorXd q, qdot, qddot;
-    auto car_task = solver->getTask("base_link");
-    auto car_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(car_task);    
-
-  
-
-
-    double det_shift_x = 0.5, det_shift_y = 0.5;
-
-    ros::Rate r(10);
     ros::ServiceServer service = nodeHandle.advertiseService("start_searching", start_searching);
+    ros::Rate r(10);
 
-    geometry_msgs::TransformStamped tag_base_T; 
-    geometry_msgs::TransformStamped wheelFL_base_T; 
+
+
+    bool search_flag = false;
+    int off_num = 25;
+    // error and gain
     double roll_e, pitch_e, yaw_e;
+    double K_x = 0.1, K_y = 0.2, K_yaw = 0.1;
+    // time
+    
+    double time = 0 ;
+    //frame name
+    std::string parent_frame = "base_link";
+    std::string child_frame = "tag_0";
+
+    // error matrix
     Eigen::Vector6d E;
     Eigen::Vector6d E_Zero;
     E_Zero.setZero();
-    double K_x = 0.1, K_y = 0.2, K_roll = 0.1, K_pitch = 0.1 , K_yaw = 0.1;
-    bool reach_goal = false;
+    Eigen::VectorXd q, qdot, qddot;
+    // tag to base translation
+    geometry_msgs::TransformStamped tag_base_T; 
 
-    int nodes_num = 100;
-
-    int off_num = 100; // off num to start tracking
-    double yaw_l = -0.5;
-    double yaw_h = 0.5;
-    bool reach_yaw = false;
-
-    // std::cout << "robot->arm(1).getBaseLinkName() = " << robot->arm(0).getBaseLinkName() << std::endl;
-    // robot->getChainMap
-    std::cout << "motion chain names: " << std::endl << std::endl;
+    auto car_task = solver->getTask("base_link");
+    auto car_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(car_task);
     for (size_t i = 0; i < robot->getChainNames().size(); i++)
     {
         std::cout << "Chain [" << i <<"] = " << robot->getChainNames()[i] << std::endl;
     }
-    
+
+
 
     while (ros::ok())
     {
@@ -203,28 +171,116 @@ int main(int argc, char **argv)
             ros::spinOnce();
             r.sleep();
         }
+       
+        if(reach_goal_1){
+            if (turn_num)
+                TurnAround(car_cartesian.get(), turn_num);
+            if (turn_num == 0)
+            {
+                /* code */
+            
+            
+            /**
+             * 1 no tagDetected and need to search tag
+            */
+            if (!tagDetected_1 && !search_flag_2){
+                std::cout << "2 no tagDetected and need to search tag" << std::endl;
+                TurnToTag(car_cartesian.get(), -1);
+            } 
+            /**
+             * 2. tagDetected but need to turn to tag
+             * */ 
+            if (tagDetected_1 && off_num_2 ){
+                Eigen::Vector6d E;
+                double yaw_e = -3.14 * 1/5;
+                E[0] = 0;
+                E[1] = 0;
+                E[2] = 0;
+                E[3] = 0;
+                E[4] = 0;
+                E[5] = 0.2 * yaw_e;
+                car_cartesian->setVelocityReference(E); 
+                off_num_2 -- ;
+                std::cout << "2 tagDetected but need to turn to tag: " << off_num_2 << std::endl;
+            }
+
+            /**
+             * 3. if tagDetected and have turn toward tag
+             * */ 
+            if (tagDetected_1 && !off_num_2 )
+            {
+                std::cout << "2 tagDetected_1 = " << tagDetected_1 << std::endl;
+                search_flag_2 = true;
+                tag_base_T = tfBuffer.lookupTransform(parent_frame, child_frame, ros::Time(0));
+                /**
+                 * Error Calculate
+                */
+                double x_e = tag_base_T.transform.translation.x;
+                double y_e = tag_base_T.transform.translation.y ;
+                tf2::Quaternion q_;
+                q_.setW(tag_base_T.transform.rotation.w);
+                q_.setX(tag_base_T.transform.rotation.x);
+                q_.setY(tag_base_T.transform.rotation.y);
+                q_.setZ(tag_base_T.transform.rotation.z);
+                tf2::Matrix3x3 m(q_);
+                m.getRPY(roll_e, pitch_e, yaw_e);
+                yaw_e = yaw_e + 1.6;
+                /**
+                 * Velocity Controller
+                */
+                E[0] = K_x * x_e;
+                E[1] = K_y * y_e;
+                E[2] = 0;
+                E[3] = 0;
+                E[4] = 0;
+                E[5] = K_yaw * 0;
+                /**
+                 * 2.1 large error need to move it to tag
+                */
+                if ((abs(x_e) > 1 || abs(y_e) > 0.4 ) && !reach_goal_2)
+                {                
+                    std::cout << "2x_e = " << x_e << std::endl;
+                    std::cout << "2y_e = " << y_e << std::endl;
+                    car_cartesian->setVelocityReference(E);
+                }else{
+                    std::cout << "2reach_goal_2" << std::endl;
+                    reach_goal_2 = true;
+                }
+            }
+        }
+        }else{
+
+
+
         /**
-         * Detection
+         * 1 no tagDetected and need to search tag
         */
-        std::string parent_frame = "base_link";
-        std::string child_frame = "tag_0";
-        if (tagDetected && off_num ){
+        if (!tagDetected_1 && !search_flag){
+            std::cout << "no tagDetected and need to search tag" << std::endl;
+            TurnToTag(car_cartesian.get(), 1);
+        } 
+        /**
+         * 2. tagDetected but need to turn to tag
+         * */ 
+        if (tagDetected_1 && off_num ){
+            Eigen::Vector6d E;
+            double yaw_e = 3.14 * 1/5;
             E[0] = 0;
             E[1] = 0;
             E[2] = 0;
             E[3] = 0;
             E[4] = 0;
-            double yaw_e = 3.14 * (100 - off_num)/200;
-            E[5] = K_yaw * yaw_e;
+            E[5] = 0.2 * yaw_e;
             car_cartesian->setVelocityReference(E); 
             off_num -- ;
-            std::cout << "tagDetected but should turn it to tag off num = " << off_num << std::endl;
+            std::cout << "tagDetected but need to turn to tag" << std::endl;
         }
-
-        if (tagDetected && !off_num )
+        /**
+         * 3. if tagDetected and have turn toward tag
+         * */ 
+        if (tagDetected_1 && !off_num )
         {
-
-            std::cout << "tagDetected = " << tagDetected << std::endl;
+            std::cout << "tagDetected_1 = " << tagDetected_1 << std::endl;
             search_flag = true;
             tag_base_T = tfBuffer.lookupTransform(parent_frame, child_frame, ros::Time(0));
             /**
@@ -232,56 +288,37 @@ int main(int argc, char **argv)
             */
             double x_e = tag_base_T.transform.translation.x;
             double y_e = tag_base_T.transform.translation.y ;
-            // std::cout << "y_e = " << y_e <<  std::endl;
-            double z_e = tag_base_T.transform.translation.z;
-
-            double x_ee = tag_base_T.transform.translation.x * tag_base_T.transform.translation.x;
-            double y_ee = tag_base_T.transform.translation.y * tag_base_T.transform.translation.y;
-            double z_ee = tag_base_T.transform.translation.z * tag_base_T.transform.translation.z;
-            double e = sqrt(x_ee + y_ee);
-
             tf2::Quaternion q_;
             q_.setW(tag_base_T.transform.rotation.w);
             q_.setX(tag_base_T.transform.rotation.x);
             q_.setY(tag_base_T.transform.rotation.y);
             q_.setZ(tag_base_T.transform.rotation.z);
-            
             tf2::Matrix3x3 m(q_);
             m.getRPY(roll_e, pitch_e, yaw_e);
             yaw_e = yaw_e + 1.6;
             /**
              * Velocity Controller
             */
-            
-            
             E[0] = K_x * x_e;
             E[1] = K_y * y_e;
             E[2] = 0;
             E[3] = 0;
             E[4] = 0;
             E[5] = K_yaw * 0;
-            // std::cout << "yaw_error = " << E[5] << std::endl;
-            // E = K * E * e;
-
-
-            // std::cout << "tag_base_T.transform.translation.x = " << tag_base_T.transform.translation.x << std::endl;
-            // std::cout << "tag_base_T.transform.translation.y = " << tag_base_T.transform.translation.y << std::endl;
-            // std::cout << "tag_base_T.transform.translation.z = " << tag_base_T.transform.translation.z << std::endl;
-            // std::cout << "yaw = " << yaw_e << std::endl;
-
-                if ((abs(x_e) > 1 || abs(y_e) > 0.4 ) && !reach_goal)
-                {                
-                    std::cout << "x_e = " << x_e << std::endl;
-                    std::cout << "y_e = " << y_e << std::endl;
-                    // std::cout << "yaw_e = " << yaw_e << std::endl;
-                    car_cartesian->setVelocityReference(E);
-                }
+            /**
+             * 2.1 large error need to move it to tag
+            */
+            if ((abs(x_e) > 1 || abs(y_e) > 0.4 ) && !reach_goal_1)
+            {                
+                std::cout << "x_e = " << x_e << std::endl;
+                std::cout << "y_e = " << y_e << std::endl;
+                car_cartesian->setVelocityReference(E);
+            }else{
+                std::cout << "reach_goal_1" << std::endl;
+                reach_goal_1 = true;
             }
-            
-        if (!tagDetected && !search_flag){
-            std::cout << "no tag" << std::endl;
-            TurnToTag(car_cartesian.get());
-        } 
+         }
+        }
             solver->update(time, dt);
             model->getJointPosition(q);
             model->getJointVelocity(qdot);
